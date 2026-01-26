57e17460b9f3ff9ef92c5790a87f30ac27f3efff
Nyxh94 <nja210@exeter.ac.uk>
2026-01-26 16:22:14 +0000

    HelloWorld coding

--- ecm1410-workshop-2-main/HelloWorldApp.java
+++ ecm1410-workshop-2-main/HelloWorldApp.java
@@ -1,3 +1,14 @@
-// Add your code below
+// HelloWorld application
+public class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.print("Hello World! My name is ");
+        System.out.print(args[0]);
+        System.out.print(". ");
+        System.out.print("I am ");
+        System.out.print(args[1]);
+        System.out.println(" years old.");
+    }
+}
+
 
 
